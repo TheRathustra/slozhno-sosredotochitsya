388672e083a348c267525ced9f34c34070e2074b
Konstantin Maksimov <Purple-Pleb@yandex.ru>
2024-05-01 16:51:02 +0300

    feat: верстка без светлой темы

--- gulpfile.js
+++ gulpfile.js
@@ -33,6 +33,17 @@ function images() {
     .pipe(browserSync.reload({stream: true}));
 }
 
+function fonts() {
+  return gulp.src('src/fonts/**/*.{ttf,woff,woff2}')
+    .pipe(gulp.dest('dist/fonts'))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+function scripts() {
+  return gulp.src('src/scripts/**/*.js')
+    .pipe(gulp.dest('dist/scripts'))
+}
+
 function clean() {
   return del('dist');
 }
@@ -40,17 +51,20 @@ function clean() {
 function watchFiles() {
   gulp.watch(['src/**/*.html'], html);
   gulp.watch(['src/**/*.css'], css);
+  gulp.watch(['src/fonts/**/*.{ttf,woff,woff2}'], fonts);
   gulp.watch(['src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
 }
 
-const build = gulp.series(clean, gulp.parallel(html, css, images));
+const build = gulp.series(clean, gulp.parallel(html, css, fonts, scripts, images));
 const watchapp = gulp.parallel(build, watchFiles, serve);
 
 exports.html = html;
 exports.css = css;
 exports.images = images;
 exports.clean = clean;
+exports.fonts = fonts;
 
+exports.scripts = scripts;
 exports.build = build;
 exports.watchapp = watchapp;
 exports.default = watchapp;
